9b3223f6697ea5642b11045af7860bf3b5b44049
Aluru Murali Krishna <alurumuralikrishna9@gmail.com>
2022-05-17 01:48:50 +0530

    Added one-one, group chat functionality

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const chats = require("./data/data");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middlewares/erroMiddleware");
 
 const app = express();
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.get("/api/chats", (req, res) => {
   res.send(chats);
